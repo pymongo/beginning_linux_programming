bc0a5097226e4e712f2cd147636f87944629e3a0
wuaoxiang <os.popen@gmail.com>
2021-07-08 10:59:35 +0800

    set syslog max log level

--- src/bin/syslog.rs
+++ src/bin/syslog.rs
@@ -18,11 +18,26 @@ fn print_all_syslog_level() {
     dbg!(libc::LOG_DEBUG);
 }
 
+/// syslog.h LOG_UPTO
+fn log_upto(priority: i32) -> i32 {
+    (1 << ((priority)+1)) - 1
+}
+
+/// journalctl -perr --output=json-pretty -f
 unsafe fn _main() {
+
+    // ident is syslog unit name
+    libc::openlog("syslog.rs\0".as_ptr().cast(), libc::LOG_PID | libc::LOG_CONS, libc::LOG_USER);
+
     // %m is not a printf format, is a syslog addition
     libc::syslog(
-        libc::LOG_ALERT | libc::LOG_USER,
+        libc::LOG_ERR | libc::LOG_USER,
         "hello %d, the errmsg of errno=%m\0".as_ptr().cast(),
         1,
     );
+
+    // set max log level to libc::LOG_CRIT
+    libc::setlogmask(log_upto(libc::LOG_CRIT));
+    // priority > libc::LOG_CRIT would be ignore
+    libc::syslog(libc::LOG_ERR, "would not show\0".as_ptr().cast());
 }
