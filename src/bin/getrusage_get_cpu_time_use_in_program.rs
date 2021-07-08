710bd57c6be97a106784df959796635ed32a2b74
wuaoxiang <os.popen@gmail.com>
2021-07-08 14:06:54 +0800

    add getrusage syscall example

---
+++ src/bin/getrusage_get_cpu_time_use_in_program.rs
@@ -0,0 +1,24 @@
+#![warn(clippy::nursery, clippy::pedantic)]
+
+fn main() {
+    unsafe {
+        _main();
+    }
+}
+
+unsafe fn _main() {
+    let mut rusage = std::mem::zeroed();
+    // libc::RUSAGE_SELF: usage information about current program
+    // libc::RUSAGE_CHILDREN: usage information about current program and child processes as well
+    libc::getrusage(libc::RUSAGE_SELF, &mut rusage);
+
+    println!("user space time second:");
+    dbg!(rusage.ru_utime.tv_sec);
+    println!("user space time microseconds:");
+    dbg!(rusage.ru_utime.tv_usec);
+    println!("system/kernel space time second:");
+    dbg!(rusage.ru_stime.tv_sec);
+    println!("system/kernel space time microseconds:");
+    dbg!(rusage.ru_stime.tv_usec);
+
+}
---
+++ src/bin/temp.rs
@@ -0,0 +1,24 @@
+fn main() {
+    unsafe {
+        _main();
+    }
+}
+
+/// 由于 -1 是一个 valid 的 getpriority 返回值，所以只能先把errno设置成0再调用getpriority看errno有没有变成其他错误码
+/// errno should be set to zero before calling getpriority and checked that it’s still zero
+unsafe fn _main() {
+    *libc::__errno_location() = 0;
+    // default process_priority is 0, priority range [-20, 20], -20 is the highest priority
+    let systemd_process_priority = libc::getpriority(libc::PRIO_PROCESS, 1);
+    if *libc::__errno_location() > 0 {
+        libc::perror("getpriority\0".as_ptr().cast());
+    }
+    dbg!(systemd_process_priority);
+
+    *libc::__errno_location() = 0;
+    let current_process_priority = libc::getpriority(libc::PRIO_PROCESS, libc::getpid() as u32);
+    if *libc::__errno_location() > 0 {
+        libc::perror("getpriority\0".as_ptr().cast());
+    }
+    dbg!(current_process_priority);
+}
