f50df5b76715e5f5454c89b1a511dabb5dbcc7c3
wuaoxiang <os.popen@gmail.com>
2021-07-14 09:49:31 +0800

    add wait and zombie_child_process

---
+++ src/bin/zombie_child_process.rs
@@ -0,0 +1,49 @@
+#![warn(clippy::nursery, clippy::pedantic)]
+
+fn main() {
+    let mut msg = "parent process";
+    let mut n_times = 4;
+    let mut child_process_pid = -1;
+    let mut exit_code = libc::EXIT_SUCCESS;
+    println!("before fork");
+
+    match unsafe { libc::fork() } {
+        -1 => unsafe {
+            libc::perror("fork\0".as_ptr().cast());
+            libc::exit(libc::EXIT_FAILURE);
+        },
+        0 => {
+            msg = "child process";
+            // child exit while parent still sleeping
+            // but parent need child keep it's exit_code until parent call wait
+            // so child is a zombie process now
+            n_times = 1;
+            exit_code = 37;
+        }
+        fork_ret_child_process_pid => {
+            child_process_pid = fork_ret_child_process_pid;
+        }
+    }
+
+    // parent process is exit before child process finished
+    for _ in 0..n_times {
+        println!("{}", msg);
+        unsafe {
+            libc::sleep(1);
+        }
+    }
+
+    // parent
+    if child_process_pid != -1 {
+        let mut wait_ret_child_process_status = 0;
+        let wait_ret_child_process_pid = unsafe { libc::wait(&mut wait_ret_child_process_status) };
+        assert_eq!(child_process_pid, wait_ret_child_process_pid);
+        if libc::WIFEXITED(wait_ret_child_process_status) {
+            println!("child exit code = {}", libc::WEXITSTATUS(wait_ret_child_process_status));
+        } else {
+            println!("child process exit abnormally");
+        }
+    }
+
+    unsafe { libc::exit(exit_code); }
+}
