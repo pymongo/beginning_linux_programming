5d239b7e976c19c8bd7432615b1bd5b9a7b6d05e
wuaoxiang <os.popen@gmail.com>
2021-07-18 14:39:14 +0800

    add inet_aton example

--- src/bin/socket_1_local_socket_file.rs
+++ src/bin/socket_1_local_socket_file.rs
@@ -29,9 +29,19 @@ const SERVER_SOCKET_FILENAME: [u8; 14] = *b"server_socket\0";
 ## server echo but uppercase client request
 request: single byte
 response: uppercase of request byte
+
+## C10k problem
+首先 BLOCKING 的 read/write 会阻塞线程所以同时只能处理一个 client_socket
+其次每个进程最多能开 1024 个 fd，如果 10 万个客户端长链接怎么办?
+```text
+[w@ww bash_programming]$ ulimit -a
+real-time non-blocking time  (microseconds, -R) unlimited
+// ...
+open files                          (-n) 1024
+```
 */
 unsafe fn server() {
-    /* 1. socket()
+    /* 1. socket(): create a socket endpoint for communication
     fn socket(domain: i32, type: i32, protocol: i32) -> i32: create an endpoint for communication
     domain: AF_UNIX == AF_LOCK, local communication
     type: SOCKET_STREAM Provides sequenced, reliable, two-way, connection-based byte streams. An out-of-band data transmission mechanism may be supported.
@@ -41,7 +51,8 @@ unsafe fn server() {
     assert_ne!(server_socket_fd, -1);
     dbg!(server_socket_fd);
 
-    // 2. bind
+    // 2. bind(): bind a name(file?) to a socket(fd)
+    // AF_UNIX 的 地址最好用 sockaddr_un 的结构体，跟 AF_INET 用的 sockaddr_in 区分开
     let mut server_addr: libc::sockaddr_un = std::mem::zeroed();
     server_addr.sun_family = libc::AF_UNIX as u16;
     libc::strcpy(
@@ -111,16 +122,17 @@ unsafe fn client() {
     dbg!(socket_fd);
 
     // 2. connect(socket_fd, sockaddr)
-    let server_addr = libc::sockaddr {
-        sa_family: libc::AF_UNIX as u16,
-        sa_data: std::mem::transmute(SERVER_SOCKET_FILENAME),
-    };
-    // let mut server_addr: libc::sockaddr_un = std::mem::zeroed();
-    // server_addr.sun_family = libc::AF_UNIX as u16;
-    // libc::strcpy(server_addr.sun_path.as_mut_ptr(), SERVER_SOCKET_FILENAME.as_ptr().cast());
+    // AF_UNIX 的 地址最好用 sockaddr_un 的结构体再 cast 成 sockaddr，跟 AF_INET 用的 sockaddr_in 区分开
+    // let server_addr = libc::sockaddr {
+    //     sa_family: libc::AF_UNIX as u16,
+    //     sa_data: std::mem::transmute(SERVER_SOCKET_FILENAME),
+    // };
+    let mut server_addr: libc::sockaddr_un = std::mem::zeroed();
+    server_addr.sun_family = libc::AF_UNIX as u16;
+    libc::strcpy(server_addr.sun_path.as_mut_ptr(), SERVER_SOCKET_FILENAME.as_ptr().cast());
     let connect_res = libc::connect(
         socket_fd,
-        &server_addr,
+        (&server_addr as *const libc::sockaddr_un).cast(),
         std::mem::size_of_val(&server_addr) as libc::socklen_t,
     );
     if connect_res == -1 {
