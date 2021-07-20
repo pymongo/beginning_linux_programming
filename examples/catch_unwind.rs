414f5aa7e0e45498e9aace3df1e9d87bde54a708
wuaoxiang <os.popen@gmail.com>
2021-07-20 11:27:00 +0800

    add gtk button signal example

---
+++ examples/catch_unwind.rs
@@ -0,0 +1,14 @@
+fn main() {
+    assert!(std::panic::catch_unwind(|| {
+        panic!("oh no");
+    }).is_err());
+    println!("程序从闭包代码的 panic(\"oh no\") 这行中恢复过来，并继续往后执行");
+}
+
+trait A {}
+
+impl std::fmt::Debug for dyn A {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        todo!()
+    }
+}
---
+++ gtk/gtk_2_button_signal_callback.c
@@ -0,0 +1,22 @@
+#include <gtk/gtk.h>
+#include <stdio.h>
+
+void button_clicked_callback(GtkWidget *button, gpointer data) {
+    printf("event=clicked, button receive data = %s\n", (char *) data);
+}
+
+int main(int argc, char *argv[]) {
+    gtk_init(&argc, &argv);
+
+    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
+
+    GtkWidget *button = gtk_button_new_with_label("my_button");
+    gtk_container_add(GTK_CONTAINER(window), button);
+    g_signal_connect(button, "clicked", G_CALLBACK(button_clicked_callback), "user input data send to button");
+
+    gtk_widget_show(window);
+    gtk_widget_show(button);
+    gtk_main();
+    return 0;
+}
