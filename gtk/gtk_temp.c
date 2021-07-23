f0dd26d50b895cd8cad400080178fbfcae1ae3b2
wuaoxiang <os.popen@gmail.com>
2021-07-23 17:18:47 +0800

    update ping

--- gtk/gtk_temp.c
+++ gtk/gtk_temp.c
@@ -1,5 +1,4 @@
 #include <fcntl.h>
-#include <errno.h>
 #include <sys/socket.h>
 #include <resolv.h>
 #include <netdb.h>
@@ -9,19 +8,12 @@
 #include <unistd.h>
 #include <string.h>
 
-#define PACKETSIZE  64
 struct packet {
     struct icmphdr hdr;
-    char msg[PACKETSIZE - sizeof(struct icmphdr)];
+    char msg[64 - sizeof(struct icmphdr)];
 };
 
-int pid = -1;
-struct protoent *proto = NULL;
-int cnt = 1;
-
-/*--------------------------------------------------------------------*/
-/*--- checksum - standard 1s complement checksum                   ---*/
-/*--------------------------------------------------------------------*/
+// checksum - standard 1s complement checksum
 unsigned short checksum(void *b, int len) {
     unsigned short *buf = b;
     unsigned int sum = 0;
@@ -37,67 +29,58 @@ unsigned short checksum(void *b, int len) {
     return result;
 }
 
-
-/*--------------------------------------------------------------------*/
-/*--- ping - Create message and send it.                           ---*/
-/*    return 0 is ping Ok, return 1 is ping not OK.                ---*/
-/*--------------------------------------------------------------------*/
 bool ping(char *adress) {
-    const int val = 255;
-    int i, sd;
-    struct packet pckt;
     struct sockaddr_in r_addr;
-    int loop;
-    struct hostent *hname;
     struct sockaddr_in addr_ping, *addr;
-
-    pid = getpid();
-    proto = getprotobyname("ICMP");
-    hname = gethostbyname(adress);
+    struct hostent *hname = gethostbyname(adress);
     bzero(&addr_ping, sizeof(addr_ping));
     addr_ping.sin_family = hname->h_addrtype;
     addr_ping.sin_port = 0;
     addr_ping.sin_addr.s_addr = *(long *) hname->h_addr;
-
     addr = &addr_ping;
 
-    sd = socket(PF_INET, SOCK_RAW, proto->p_proto);
+    int sd = socket(PF_INET, SOCK_RAW, IPPROTO_ICMP);
     if (sd < 0) {
         perror("socket");
         return false;
     }
-    if (setsockopt(sd, SOL_IP, IP_TTL, &val, sizeof(val)) != 0) {
-        perror("Set TTL option");
+    if (fcntl(sd, F_SETFL, O_NONBLOCK) != 0) {
+        perror("fcntl");
         return false;
     }
-    if (fcntl(sd, F_SETFL, O_NONBLOCK) != 0) {
-        perror("Request nonblocking I/O");
+    const int ttl_val = 64;
+    if (setsockopt(sd, SOL_IP, IP_TTL, &ttl_val, sizeof(ttl_val)) != 0) {
+        perror("setsockopt");
         return false;
     }
 
-    for (loop = 0; loop < 10; loop++) {
+//    int cnt = 1;
+    for (int loop = 0; loop < 10; loop++) {
         int len = sizeof(r_addr);
+        printf("before recvfrom\n");
+        struct packet pckt;
         if (recvfrom(sd, &pckt, sizeof(pckt), 0, (struct sockaddr *) &r_addr, &len) > 0) {
             return true;
         }
+        printf("after recvfrom\n");
 
         bzero(&pckt, sizeof(pckt));
         pckt.hdr.type = ICMP_ECHO;
-        pckt.hdr.un.echo.id = pid;
-        for (i = 0; i < sizeof(pckt.msg) - 1; i++)
+//        int i = 0;
+        for (int i = 0; i < sizeof(pckt.msg) - 1; i++)
             pckt.msg[i] = i + '0';
-        pckt.msg[i] = 0;
-        pckt.hdr.un.echo.sequence = cnt++;
+//        pckt.msg[i] = 0;
+//        pckt.hdr.un.echo.sequence = cnt++;
         pckt.hdr.checksum = checksum(&pckt, sizeof(pckt));
         if (sendto(sd, &pckt, sizeof(pckt), 0, (struct sockaddr *) addr, sizeof(*addr)) <= 0)
             perror("sendto");
-        usleep(300*1000);
+        usleep(300 * 1000);
     }
     return false;
 }
 
 int main(int argc, char *argv[]) {
-    if (ping("www.rust-lang.com") == true) {
+    if (ping("www.baidu.com") == true) {
         printf("Ping is OK. \n");
     } else {
         printf("Ping is not OK. \n");
