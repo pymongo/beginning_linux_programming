667f396dde85f31923339e55999c19f4204f9b7d
wuaoxiang <os.popen@gmail.com>
2021-07-27 11:33:25 +0800

    add qt Modal dialog to qt_02 example

--- qt/qt_02_moc_button_signal_slot/mainwindow.cpp
+++ qt/qt_02_moc_button_signal_slot/mainwindow.cpp
@@ -1,21 +1,36 @@
 #include "mainwindow.h"
 #include <QPushButton>
 #include <QDebug>
-#include <iostream>
+#include <QDialog>
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-{
+    : QMainWindow(parent) {
     setWindowTitle("My window title");
+    resize(400, 300);
     auto button = new QPushButton("button_label", this);
-    connect(button, SIGNAL(clicked()), this, SLOT(my_clicked()));
+    button->setGeometry(200-120/2, 150-60/2, 120, 60);
+    connect(button, SIGNAL(clicked()), this, SLOT(clicked_handler()));
 }
 
-MainWindow::~MainWindow()
-{
-}
+MainWindow::~MainWindow() {}
 
-void MainWindow::my_clicked() {
+void MainWindow::clicked_handler() {
     // QDebug auto add newline
     qWarning() << "button clicked!";
+    auto dialog = new QDialog(this);
+    dialog->resize(400, 300);
+    dialog->setWindowTitle("dialog");
+    int dialog_res = dialog->exec();
+    switch (dialog_res) {
+    case QDialog::DialogCode::Rejected:
+        qWarning() << "You close the dialog!";
+        qWarning() << "QDialog::DialogCode::Rejected";
+        break;
+    case QDialog::DialogCode::Accepted:
+        qWarning() << "QDialog::DialogCode::Accepted";
+        break;
+    default:
+        qWarning() << "dialog_res =" << dialog_res;
+        break;
+    }
 }
--- qt/qt_02_moc_button_signal_slot/mainwindow.h
+++ qt/qt_02_moc_button_signal_slot/mainwindow.h
@@ -3,14 +3,13 @@
 
 #include <QMainWindow>
 
-class MainWindow : public QMainWindow
-{
+class MainWindow : public QMainWindow {
     Q_OBJECT
 
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 private slots:
-    void my_clicked();
+    void clicked_handler();
 };
 #endif // MAINWINDOW_H
