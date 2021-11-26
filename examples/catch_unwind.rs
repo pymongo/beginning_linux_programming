382f4f06450d2060249d2b2966d6f07647f2f4d8
wuaoxiang <wuaoxiang@stargraph.cn>
2021-11-26 17:25:05 +0800

    move some examples to learn_rust repo

--- examples/catch_unwind.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    // catch_unwind 类似其它语言 try..catch 的 catch
-    assert!(std::panic::catch_unwind(|| {
-        panic!("oh no");
-    })
-    .is_err());
-    println!("程序从闭包代码的 panic(\"oh no\") 这行中恢复过来，并继续往后执行");
-}
